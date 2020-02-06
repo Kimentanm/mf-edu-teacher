c9ece776fd2685c17da5973c476442f308841f16
Kimen <Kimentanm@gmail.com>
2020-02-06 21:11:57 +0800

    some changes

--- src/views/class/ppt-upload.vue
+++ src/views/class/ppt-upload.vue
@@ -11,7 +11,7 @@
             :on-exceeded-size="handleExceededSize"
             :on-success="handleUploadSuccess">
         <div class="ppt-upload-tag">
-            <img src="http://img.kimen.com.cn/PPT.png">
+            <img src="../../assets/images/ppt-icon.png">
             <p>点击或拖拽上传PPT</p>
         </div>
 
